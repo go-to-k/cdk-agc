67f7148a528919817933eebe888dbc38d84e51e8
go-to-k <24818752+go-to-k@users.noreply.github.com>
2026-02-10 02:50:00 +0900

    integ

--- test-cdk/app.ts
+++ test-cdk/app.ts
@@ -2,7 +2,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Stack } from "aws-cdk-lib";
 import type { StackProps } from "aws-cdk-lib";
-import * as s3 from "aws-cdk-lib/aws-s3";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as ecr_assets from "aws-cdk-lib/aws-ecr-assets";
 import type { Construct } from "constructs";
@@ -16,29 +15,11 @@ class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // S3 Bucket with large assets
-    new s3.Bucket(this, "TestBucket", {
-      versioned: true,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-    });
-
     // Lambda with bundled code (creates asset)
     new lambda.Function(this, "BundledFunction", {
       runtime: lambda.Runtime.NODEJS_24_X,
       handler: "index.handler",
       code: lambda.Code.fromAsset(path.join(__dirname, "lambda")),
-    });
-
-    // Another Lambda to generate more assets
-    new lambda.Function(this, "InlineFunction", {
-      runtime: lambda.Runtime.NODEJS_24_X,
-      handler: "index.handler",
-      code: lambda.Code.fromInline(`
-        exports.handler = async (event) => {
-          console.log('Hello from Lambda!');
-          return { statusCode: 200, body: 'OK' };
-        };
-      `),
     });
 
     // Docker image asset (creates dockerImages entry in *.assets.json)
